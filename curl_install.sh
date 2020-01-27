e8f1b923a11aabd932addd8b38ac1e232d386b76
Abhishek <abhi.una12@gmail.com>
2020-01-27 22:00:05 +0530

    Added custom curl with sftp

---
+++ curl_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get purge curl -y; 
+wget https://curl.haxx.se/download/curl-7.68.0.tar.gz
+tar xf curl-7.68.0.tar.gz
+cd curl-7.68.0
+./configure --with-libssh2
+make -j$(nproc)
+sudo make install
