291501731bf9e95493a5d3ac8dffa5ab3a4dbd31
Abhishek <abhi.una12@gmail.com>
2020-01-27 22:25:40 +0530

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -k  -u $USER:$PASSWORD -T "$HOME"/test.txt sftp://frs.sourceforge.net:/home/pfs/project/testing_sf
+curl -k  -u $USER:$PASSWORD -T "$HOME"/test.txt sftp://frs.sourceforge.net/home/pfs/project/testing-sf
